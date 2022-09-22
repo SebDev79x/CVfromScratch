2876965cc5ea18a5ba5ab02b3bfa5820de1c5885
SebDev79x <sebastien.mazur@wanadoo.fr>
2022-09-22 07:35:20 +0200

    Swiper implémenté, fonctionnel, responsive, proportion h2 ok

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -9,23 +9,7 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView:2,
-      spaceBetween: 40
-    }
-},
+
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar'
--- components/formations.php
+++ components/formations.php
@@ -2,9 +2,8 @@
 
     <!-- Slider main container -->
 
-<div class="swiperTest">
     <div class="swiper">
-        <div class="diplomasTitle">
+    <div class="diplomasTitle">
             <h2>Diplômes</h2>
         </div>
         <div class="swiper-wrapper">
@@ -59,48 +58,4 @@
         <div class="swiper-button-next"></div>
         <div class="swiper-scrollbar"></div>
     </div>
-
-    </div>
 </section>
-<!-- <div class="diplomas" id="diplomas">
-            <div class="firstDiploma diploma" id="firstDiploma">
-                <h3>BACCALAURÉAT</br>Sciences Eco. & Sociales</h3>
-                <ul>
-                    <li>1997</li>
-                    <li>Lycéen Jean Calvin</li>
-                    <li>Noyon, 60</li>
-                </ul>
-            </div>
-            <div class="secondDiploma diploma" id="secondDiploma">
-                <h3>B.T.S.</br>
-                    Assistant de Gestion PME/PMI</h3>
-                <ul>
-                    <li>2003</li>
-                    <li>AFPI Sifor Oise</li>
-                    <li>Compiègne, 60</li>
-                </ul>
-            </div>
-            <div class="thirdDiploma diploma" id="thirdDiploma">
-                <h3>TITRE</br>
-                    Développeur Web & Web Mobile</h3>
-                <ul>
-                    <li>2020</li>
-                    <li>La Manu</li>
-                    <li>Noyon, 60</li>
-                </ul>
-            </div>
-            <div class="fourthDiploma diploma" id="fourthDiploma">
-                <h3>CERTIFICAT</br>
-                    React Native</h3>
-                <ul>
-                    <li>2022</li>
-                    <li>La Manu</li>
-                    <li>Compiègne, 60</li>
-                </ul>
-            </div>
-        </div> -->
-<!--      <div class="lineChrono" id="lineChrono"></div>
-    </div>
-    <div>
-        <button id="btnChrono" type="button" class="btn btn-outline-warning">Warning</button>
-    </div> -->
