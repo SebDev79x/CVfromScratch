0609df41b3bcb157aac4bee6bddff97231bd121f
SebDev79x <sebastien.mazur@wanadoo.fr>
2022-10-03 09:06:58 +0200

    Downloadable CV

--- components/info.php
+++ components/info.php
@@ -9,7 +9,7 @@
                 J'ai obtenu le titre de Développeur Web & Web Mobile en 2020 puis j'ai poursuivi mon apprentissage
                 en autodidacte, codant jour après jour.</br>
                 J'ai également suivi une formation React Native de 3 mois, achevée fin juillet 2022.</br>
-            Le tout via <a href="https://lamanu.fr/" class="lamanuLink">La Manu</a>.</p>
+                Le tout via <a href="https://lamanu.fr/" class="lamanuLink">La Manu</a>.</p>
         </div>
     </article>
     <article class="articleInfo item item2">
@@ -17,12 +17,12 @@
             <h2>Savoir-être</h2>
         </div>
         <div class="item2Inf itemContent">
-                <ul>
-                    <li>Esprit d'équipe</li>
-                    <li>Autonome</li>
-                    <li>Discipliné</li>
-                    <li>Tenace</li>
-                </ul> 
+            <ul>
+                <li>Esprit d'équipe</li>
+                <li>Autonome</li>
+                <li>Discipliné</li>
+                <li>Tenace</li>
+            </ul>
         </div>
     </article>
     <article class="articleInfo item item3">
@@ -42,22 +42,21 @@
         </div>
         <div class="item4Inf itemContent">
             <p class="firstSentence">43 ans, célibataire sans enfant, je suis à la recherche d'opportunités
-                professionnelles, idéalement
-                en CDI.</p>
-                <p>Chargé de clientèle pendant 5 ans à Paris jusqu'en 2012, je demeure ouvert à l'idée d'y travailler
+                professionnelles, idéalement en CDI.</br>
+                Chargé de clientèle pendant 5 ans à Paris jusqu'en 2012, je demeure ouvert à l'idée d'y travailler
                 de nouveau si salaire attrayant et possibilité de télétravail, à moins de 25mn à pied de la Gare du
-                Nord.</p>
-                <p>Disponible immédiatement.
-            </p>
+                Nord.</br>
+                Disponible immédiatement.</p>
         </div>
     </article>
     <article>
-    <div class="item5Sup itemTitle">
+        <div class="item5Sup itemTitle">
             <h2>CV .pdf</h2>
         </div>
         <div class="item5Inf itemContent">
-    <div class="curriculum"><a href="" download><i class="fas fa-file-download"></i></a>
-</div>
-</div>
+            <div class="curriculum"><a href="./documents/SébastienMazurCV.pdf" download="CV_Sébastien_Mazur"><i
+                        class="fas fa-file-download"></i></a>
+            </div>
+        </div>
     </article>
 </section>
